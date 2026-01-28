e4c2975bc3cdd664b43e7b2adc879ad56f10aa10
FMXExpress <FMXExpress@users.noreply.github.com>
2026-01-28 12:50:20 -0800

    Remove selected frames toggle button

--- cvat-ui/src/components/annotation-page/top-bar/player-navigation.tsx
+++ cvat-ui/src/components/annotation-page/top-bar/player-navigation.tsx
@@ -136,7 +136,6 @@ function PlayerNavigation(props: Props): JSX.Element {
 
     const [frameInputValue, setFrameInputValue] = useState<number>(frameNumber);
     const [selectedFramesInput, setSelectedFramesInput] = useState<string>('');
-    const [selectedFramesVisible, setSelectedFramesVisible] = useState<boolean>(false);
     const dispatch = useDispatch();
     const {
         labels,
@@ -339,35 +338,26 @@ function PlayerNavigation(props: Props): JSX.Element {
                             <LinkOutlined className='cvat-player-frame-url-icon' onClick={onURLIconClick} />
                         </CVATTooltip>
                         { deleteFrameIcon }
-                        <Button
-                            type='link'
-                            size='small'
-                            className='cvat-player-selected-frames-toggle'
-                            onClick={() => setSelectedFramesVisible((value) => !value)}
-                        >
-                            {selectedFramesVisible ? 'Hide selected frames' : `Selected frames (${selectedFrames.length})`}
-                        </Button>
                     </Col>
                 </Row>
-                {selectedFramesVisible && (
-                    <Row align='middle' className='cvat-player-selected-frames-row'>
-                        <Col className='cvat-player-selected-frames-label'>
-                            <Text type='secondary'>Trace label:</Text>
-                        </Col>
-                        <Col className='cvat-player-selected-frames-label-select'>
-                            <Select
-                                value={traceLabelID}
-                                onChange={onTraceLabelChange}
-                                placeholder='Select label'
-                                options={labels
-                                    .filter((label) => label.type !== LabelType.TAG)
-                                    .map((label) => ({
-                                        value: label.id,
-                                        label: label.name,
-                                    }))}
-                                className='cvat-player-selected-frames-label-dropdown'
-                            />
-                        </Col>
+                <Row align='middle' className='cvat-player-selected-frames-row'>
+                    <Col className='cvat-player-selected-frames-label'>
+                        <Text type='secondary'>Trace label:</Text>
+                    </Col>
+                    <Col className='cvat-player-selected-frames-label-select'>
+                        <Select
+                            value={traceLabelID}
+                            onChange={onTraceLabelChange}
+                            placeholder='Select label'
+                            options={labels
+                                .filter((label) => label.type !== LabelType.TAG)
+                                .map((label) => ({
+                                    value: label.id,
+                                    label: label.name,
+                                }))}
+                            className='cvat-player-selected-frames-label-dropdown'
+                        />
+                    </Col>
                     <Col className='cvat-player-selected-frames-input'>
                         <Input
                             placeholder='Add frames (e.g., 1, 5, 20)'
@@ -403,8 +393,7 @@ function PlayerNavigation(props: Props): JSX.Element {
                             </Tag>
                         ))}
                     </Col>
-                    </Row>
-                )}
+                </Row>
             </Col>
             <Col>
                 <CVATTooltip title={`Press ${focusFrameInputShortcut} to focus here`}>
