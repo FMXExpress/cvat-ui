a6f7a9b4ec5531caf9d5d9853b53011730e75af6
FMXExpress <FMXExpress@users.noreply.github.com>
2026-01-28 13:10:30 -0800

    Remove selected frames row from player controls

--- cvat-ui/src/components/annotation-page/top-bar/top-bar.tsx
+++ cvat-ui/src/components/annotation-page/top-bar/top-bar.tsx
@@ -80,9 +80,6 @@ interface Props {
     switchNavigationBlocked(blocked: boolean): void;
     setNavigationType(navigationType: NavigationType): void;
     switchShowSearchPallet(visible: boolean): void;
-    addSelectedFrames(frames: number[]): void;
-    removeSelectedFrame(frame: number): void;
-    clearSelectedFrames(): void;
 }
 
 export default function AnnotationTopBarComponent(props: Props): JSX.Element {
@@ -149,9 +146,6 @@ export default function AnnotationTopBarComponent(props: Props): JSX.Element {
         switchNavigationBlocked,
         switchShowSearchPallet,
         showSearchFrameByName,
-        addSelectedFrames,
-        removeSelectedFrame,
-        clearSelectedFrames,
     } = props;
 
     const playerItems: [JSX.Element, number][] = [];
@@ -212,9 +206,6 @@ export default function AnnotationTopBarComponent(props: Props): JSX.Element {
             switchNavigationBlocked={switchNavigationBlocked}
             switchShowSearchPallet={switchShowSearchPallet}
             showSearchFrameByName={showSearchFrameByName}
-            onAddSelectedFrames={addSelectedFrames}
-            onRemoveSelectedFrame={removeSelectedFrame}
-            onClearSelectedFrames={clearSelectedFrames}
         />
     ), 10]);
 
